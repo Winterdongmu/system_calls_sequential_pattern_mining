36eb195194468bd16dc1f4940014f059eb5ba1e7
Winterdongmu <973047600@qq.com>
2022-08-24 04:21:22 -0400

    siganature generated

--- build_signature.py
+++ build_signature.py
@@ -18,27 +18,64 @@ for i in itemset:
     while np.nan in i:
         i.remove(np.nan)
 
-filter_number = 5
+filter_number = 7
 filter_list = []
 for i in itemset:
-    if len(i) >= 5:
+    if len(i) >= filter_number:
         filter_list.append(i)
-print(filter_list)
+# print(filter_list)
+
+# print(len(new_file_list))
+# print(set(new_file_list))
+# print(len(list(set(new_file_list))))
 signature_dict_list = []
+tmp_filename_list = []
+tmp_sequence_list = []
 for i in filter_list:
     sequence_store = []
-    for j in new_file_list:
-        tmp_list = list(set(i)&set(j))
+    for j in range(len(new_file_list)):
+        tmp_list = list(set(i)&set(new_file_list[j]))
         mark = True
         for k in i:
             if k not in tmp_list:
                 mark = False
         if mark ==True:
-            sequence_store.append(new_sequence_list[new_file_list.index(j)])
+            # print(j)
+            # print(new_sequence_list[new_file_list.index(j)])
+            sequence_store.append(new_sequence_list[j])
+
 
     signature_info = {
         'file': i,
         'sequence': sequence_store,
     }
+    tmp_filename_list.append(i)
+    tmp_sequence_list.append(sequence_store)
     signature_dict_list.append(signature_info)
-print(signature_dict_list)
+
+df3 = pd.DataFrame(signature_dict_list)
+df3.to_csv("signature.csv")
+
+index_list_tmp = []
+signature_info_filter_list = []
+for i in range(len(tmp_sequence_list)):
+    if i not in index_list_tmp:
+        index_list_tmp_tmp = []
+        index_list_tmp_tmp.append(i)
+        for j in range(i+1,len(tmp_sequence_list)):
+            if j not in index_list_tmp:
+                if tmp_sequence_list[i] == tmp_sequence_list[j]:
+                    # tmp_filename_list[i] = list(set(tmp_filename_list[i] + tmp_filename_list[j]))
+                    index_list_tmp.append(j)
+                    index_list_tmp_tmp.append(j)
+        newlist_file_name = []
+        for index in index_list_tmp_tmp:
+            newlist_file_name = list(set(newlist_file_name + tmp_filename_list[index]))
+        signature_info_filter = {
+            'file': newlist_file_name,
+            'sequence': tmp_sequence_list[i],
+        }
+        signature_info_filter_list.append(signature_info_filter)
+# print(signature_info_filter_list)
+df4 = pd.DataFrame(signature_info_filter_list)
+df4.to_csv("signature_filtered.csv")
--- test2.py
+++ test2.py
@@ -5,13 +5,26 @@ listb = [1, 2, 3, 4, 5, 6, 7]
 list1 = [1,2,6,7]
 list2 = ["5","2","1"]
 list3 = ["2", "1", "7","5"]
+list4 = ["5","2","1"]
 list_all = [listc, liste, lista, listb]
+list_all_2 = [["5","2","1"],["2", "1", "7","5"],["5","2","1"]]
+list5 = ['4309d17a3e2376639026494a47c5d4a68f481f1ab0eea5104cb58c5412e290ff.out_converted.csv', 'e3f90c676c4945fa685e6d61689d8dfb8cb4a496816b76b1eff12f63861c217c.out_converted.csv', '39c33c261899f2cb91f686aa6da234175237cd72cfcd9291a6e51cbdc86d4def.out_converted.csv', '1b1a56aec5b02355b90f911cdd27a35d099690fcbeb0e0622eaea831d64014d3.out_converted.csv', '5baf025c571ba362170e8d1767afe4f7adccbb4524874015fa9add308ccc203a.out_converted.csv', 'b86fa7422710bfb569986f8a4778f1614837acc754f105b7d90608a03ea7cbd6.out_converted.csv', '9e7902cfe3090cd8635af3c04de8f8510db26fb56b060060daede7dc5306f40e.out_converted.csv', '4ed03737803160dc63a560169c3cd224d0809e8a54cd8fa6fa85fc4a911971dc.out_converted.csv', '89a269943b201401b50356a4284cfda4d6689f63811853f46a16f947621d3bb3.out_converted.csv', 'a663c9cc2631de038106da41912cf7144a3e67a5faa783413172b0536a88c6f0.out_converted.csv', 'f12fc2ed61b61fe0be5500aaaa9707acebe5802d83895e87a0faebf77a59323c.out_converted.csv', '1f1bf32f553b925963485d8bb8cc3f0344720f9e67100d610d9e3f5f6bc002a1.out_converted.csv', '3ff6b4287e49a01724626a9e11adceee7a478aa5e5778ec139a3f9011a02f3af.out_converted.csv', 'fe377368c04b3b3fcd54968397a878ba5254f13af6e824732e9cfc23983b57bb.out_converted.csv', '912f27b311f90e81c135f2d363e06db44aaf4067501971703c6335e85a26e8bb.out_converted.csv', '851787bf4039d365178a16018edec430dd4a0d001c9c9c4cb68f8b73bf0c4d02.out_converted.csv']
+index_list = []
+list6 = ['4309d17a3e2376639026494a47c5d4a68f481f1ab0eea5104cb58c5412e290ff.out_converted.csv', 'e3f90c676c4945fa685e6d61689d8dfb8cb4a496816b76b1eff12f63861c217c.out_converted.csv', '39c33c261899f2cb91f686aa6da234175237cd72cfcd9291a6e51cbdc86d4def.out_converted.csv', '1b1a56aec5b02355b90f911cdd27a35d099690fcbeb0e0622eaea831d64014d3.out_converted.csv', '5baf025c571ba362170e8d1767afe4f7adccbb4524874015fa9add308ccc203a.out_converted.csv', 'b86fa7422710bfb569986f8a4778f1614837acc754f105b7d90608a03ea7cbd6.out_converted.csv', '9e7902cfe3090cd8635af3c04de8f8510db26fb56b060060daede7dc5306f40e.out_converted.csv', '4ed03737803160dc63a560169c3cd224d0809e8a54cd8fa6fa85fc4a911971dc.out_converted.csv', '89a269943b201401b50356a4284cfda4d6689f63811853f46a16f947621d3bb3.out_converted.csv', 'a663c9cc2631de038106da41912cf7144a3e67a5faa783413172b0536a88c6f0.out_converted.csv', 'f12fc2ed61b61fe0be5500aaaa9707acebe5802d83895e87a0faebf77a59323c.out_converted.csv', '1f1bf32f553b925963485d8bb8cc3f0344720f9e67100d610d9e3f5f6bc002a1.out_converted.csv', '3ff6b4287e49a01724626a9e11adceee7a478aa5e5778ec139a3f9011a02f3af.out_converted.csv', 'fe377368c04b3b3fcd54968397a878ba5254f13af6e824732e9cfc23983b57bb.out_converted.csv', '912f27b311f90e81c135f2d363e06db44aaf4067501971703c6335e85a26e8bb.out_converted.csv', '851787bf4039d365178a16018edec430dd4a0d001c9c9c4cb68f8b73bf0c4d02.out_converted.csv']
+# for i in range(0,8):
+#     print(i)
+#     if i not in index_list:
+#         index_list.append(i+1)
+#         print(index_list)
+#     else:
+#         print(i,i)
+if list5 == list6:
+    print(1)
 
-
-print(list(set(list2)&set(list3)))
-
-
-
+# index_list = []
+# for i in range(len(list_all)):
+#     for j in range(i+1,len(list_all)):
+#         if
 # pattern = []
 # for i in list_all:
 #     for j in range(len(i)):
